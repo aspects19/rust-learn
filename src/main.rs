c28f54537f1b60436b15bd2ca963a2a9a4d439b6
aspect19 <jeffamenya388@gmail.com>
2025-06-18 18:26:47 +0300

    Add logic to a loop that breaks on correct guess

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,28 @@ use rand;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    loop {
+        println!("Guess the number!");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    let random = rand::random_range(1..=100);
+        let random = rand::random_range(1..=100);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Unable to read your input");
+        io::stdin().read_line(&mut guess)
+            .expect("Unable to read your input");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&random) {
-        Ordering::Greater => println!("Your guess was bigger"),
-        Ordering::Equal => println!("You made the correct Guess"),
-        Ordering::Less => println!("Your guess was smaller")
+        match guess.cmp(&random) {
+            Ordering::Greater => println!("Your guess was bigger"),
+            Ordering::Equal => { 
+                println!("You made the correct Guess");
+                break;
+            },
+            Ordering::Less => println!("Your guess was smaller")
+        }
+
     }
-
+    
 }
