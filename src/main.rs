2cac4af2c484b9a90c18ffb624b8a7e1b32f2bf6
aspect19 <jeffamenya388@gmail.com>
2025-06-18 17:57:37 +0300

    Add random number from rand crate functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::io;
+use rand;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
-    
+
     let mut guess = String::new();
+
+    let random = rand::random_range(1..=100);
 
     io::stdin().read_line(&mut guess)
         .expect("Unable to read your input");
 
-    println!("You guessed {}", guess)
+    println!("You guessed {}", guess);
+    println!("Computer guessed {}", random);
 
 }
